814167bdcfe1d7c3a4d8ebae3d4091e60ef62723
Sebastian Yang <yukaiy@andrew.cmu.edu>
2019-09-17 22:03:51 -0400

    Update SkiBoard, BinaryBoard and graby holes

--- Assets/Scripts/GrabBall.cs
+++ Assets/Scripts/GrabBall.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class GrabBall : MonoBehaviour
 {
-    [SerializeField] private float maxSpeed = 0.5f;
+    [SerializeField] private float maxSpeed = 0.2f;
 
     private void OnTriggerEnter(Collider other)
     {
-        Vector3 targetPosition = transform.parent.position;
+        if (other.GetComponent<Collider>().GetType() == typeof(SphereCollider))
+        {
+            Vector3 targetPosition = transform.parent.position;
 
-        StartCoroutine(MoveBall(targetPosition, other));
+            StartCoroutine(MoveBall(targetPosition, other));
+        }
     }
 
     IEnumerator MoveBall(Vector3 target, Collider ball)
